fe9e14b841900697f40041ef05044ddf3b15e526
pinku95 <gouda.pinku664@gmail.com>
2025-02-19 12:23:22 +0530

    inSync with master

--- Mortgage/src/main/controller/UserController.kt
+++ Mortgage/src/main/controller/UserController.kt
@@ -1,10 +1,17 @@
 package main.controller
 
+import java.math.BigDecimal
+
 class UserController {
 
     // This is the controller for the user
 
     fun getName():String{
         return "krishna"
+    }
+
+    fun getInterest():BigDecimal{
+
+        return BigDecimal.ONE
     }
 }
